3cdd3733a054f2f8f03ba56c82677b92c3db3906
zystar <1803999267@qq.com>
2016-05-17 11:52:13 +0800

    从后台获取数据（未完成）

--- app/src/main/java/cn/bmob/imdemo/ui/SearchActivity.java
+++ app/src/main/java/cn/bmob/imdemo/ui/SearchActivity.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,6 +15,11 @@ import java.util.List;
 import java.util.Map;
 
 import cn.bmob.imdemo.R;
+import cn.bmob.imdemo.bean.QiangYu;
+import cn.bmob.imdemo.bean.User;
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.listener.FindListener;
+import cn.bmob.v3.listener.GetListener;
 
 /**
  * 搜索Activity
@@ -22,18 +28,22 @@ import cn.bmob.imdemo.R;
  */
 public class SearchActivity extends Activity {
 	private ListView list;
+	private SimpleAdapter simpleAdapter;
+	private ArrayList<QiangYu> mListItems = new ArrayList<QiangYu>();
 	//tab01临时数据
 	private String[] names = new String[]{"Mr.M","NiHuang","Jw","mz"};
 	private String[] descs = new String[]{"Age wrinkles the body. Quitting wrinkles the soul.",
 			"You know my loneliness is only kept for you, my sweet songs are only sung for you.","皇七子萧景琰，林殊（梅长苏）儿时挚友","禁军大统领，赤焰军旧部"};
-	private int[] imageIds = new int[]{R.drawable.mcs, R.drawable.nh,R.drawable.jw,R.drawable.mz};
+	private int[] imageIds = new int[]{R.drawable.user, R.drawable.nh,R.drawable.jw,R.drawable.mz,R.drawable.mz};
 	private int[] photos = new int[]{R.drawable.mcs2,R.drawable.nh,R.drawable.jw2,R.drawable.mz2};
+
+	String username;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search);
 		init();
-		tab01Adapter();
+		fetchData();
 	}
 	private void init(){
 		list = (ListView) findViewById(R.id.listView1);
@@ -41,19 +51,55 @@ public class SearchActivity extends Activity {
 	private void tab01Adapter(){
     	//创建list集合
 		List<Map<String, Object>> listItems = new ArrayList<Map<String, Object>>();
-		for (int i = 0; i < names.length; i++) {
+		for (int i = 0; i < mListItems.size(); i++) {
 			Map<String, Object> listItem = new HashMap<String, Object>();
 			listItem.put("imageView1",imageIds[i]);
-			listItem.put("textView1",names[i]);
-			listItem.put("desc",descs[i]);
-			listItem.put("photo",photos[i]);
+//			listItem.put("textView1",mListItems.get(i).getAuthor().toString());
+			String s = mListItems.get(i).getAuthor().getObjectId().toString();
+			listItem.put("textView1",getUserById(s));
+			listItem.put("desc",mListItems.get(i).getContent().toString());
+			listItem.put("photo",mListItems.get(i).getContentfigureurl().getUrl());
 			listItems.add(listItem);
 		}
 		//创建SimpleAdapter
-		SimpleAdapter simpleAdapter = new SimpleAdapter(this, listItems, R.layout.activity_list_view, 
-				new String[] {"textView1", "imageView1","desc","photo"}, 
+		simpleAdapter = new SimpleAdapter(this, listItems, R.layout.activity_list_view,
+				new String[] {"textView1", "imageView1","desc","photo"},
 				new int[] {R.id.textView1, R.id.imageView1, R.id.desc, R.id.imageView2});
 		//为ListView设置Adapter
 		list.setAdapter(simpleAdapter);
     }
+	public void fetchData(){
+		BmobQuery<QiangYu> query = new BmobQuery<QiangYu>();
+		query.order("createdAt");
+		query.findObjects(this, new FindListener<QiangYu>() {
+
+			@Override
+			public void onSuccess(List<QiangYu> list) {
+				mListItems.addAll(list);
+				Toast.makeText(SearchActivity.this, "总共有"+mListItems.size()+"条动态",Toast.LENGTH_SHORT).show();
+				tab01Adapter();
+			}
+
+			@Override
+			public void onError(int arg0, String arg1) {
+			}
+		});
+	}
+	public String getUserById(String s){
+		BmobQuery<User> query = new BmobQuery<User>();
+		query.getObject(this, s, new GetListener<User>() {
+
+			@Override
+			public void onSuccess(User object) {
+				username = object.getUsername().toString();
+			}
+
+			@Override
+			public void onFailure(int code, String arg0) {
+				Toast.makeText(SearchActivity.this, "获取用户名失败", Toast.LENGTH_SHORT).show();
+			}
+
+		});
+		return username;
+	}
 }
